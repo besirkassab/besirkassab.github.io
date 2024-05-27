6b50fadf7d45b156787831493c1b62b3ab99e61c
Besir Kassab <besirkassab@gmail.com>
2024-05-27 21:27:03 +0300

    html, css and js initial files added.

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("JavaScript is loaded!");
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
